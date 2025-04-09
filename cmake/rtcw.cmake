d93550f8f711bf7febd28f9295edf1a2c3926116
Boris I. Bendovsky <bibendovsky@hotmail.com>
2025-04-09 12:42:11 +0300

    Format CMake files a bit

--- cmake/rtcw.cmake
+++ cmake/rtcw.cmake
@@ -1,6 +1,5 @@
 cmake_minimum_required(VERSION 3.5.0 FATAL_ERROR)
 
-
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 #
@@ -44,73 +43,67 @@ message(STATUS "[${PROJECT_NAME}] Architecture: ${RTCW_ARCH_STRING}")
 
 # Precompiled headers.
 #
-option (RTCW_USE_PCH "Enable precompiled headers if available." ON)
-message (STATUS "[${PROJECT_NAME}] Enable precompiled headers: ${RTCW_USE_PCH}")
-
+option(RTCW_USE_PCH "Enable precompiled headers if available." ON)
+message(STATUS "[${PROJECT_NAME}] Enable precompiled headers: ${RTCW_USE_PCH}")
 
 # Multi-process compilation.
 #
-option (RTCW_MULTI_PROCESS_COMPILATION "Enable multi-process compilation if available." ON)
-message (STATUS "[${PROJECT_NAME}] Enable multi-process compilation: ${RTCW_MULTI_PROCESS_COMPILATION}")
+option(RTCW_MULTI_PROCESS_COMPILATION "Enable multi-process compilation if available." ON)
+message(STATUS "[${PROJECT_NAME}] Enable multi-process compilation: ${RTCW_MULTI_PROCESS_COMPILATION}")
 
-option (RTCW_CURL "Enable cURL." ON)
-message (STATUS "[${PROJECT_NAME}] Enable cURL: ${RTCW_CURL}")
+option(RTCW_CURL "Enable cURL." ON)
+message(STATUS "[${PROJECT_NAME}] Enable cURL: ${RTCW_CURL}")
 
-option (RTCW_CURL_STATIC "Link cURL statically." ON)
-message (STATUS "[${PROJECT_NAME}] Link cURL statically: ${RTCW_CURL_STATIC}")
-
+option(RTCW_CURL_STATIC "Link cURL statically." ON)
+message(STATUS "[${PROJECT_NAME}] Link cURL statically: ${RTCW_CURL_STATIC}")
 
 # Static linking.
 #
-option (RTCW_ENABLE_STATIC_LINKING "Enable static linking." ON)
-message (STATUS "[${PROJECT_NAME}] Enable static linking: ${RTCW_ENABLE_STATIC_LINKING}")
+option(RTCW_ENABLE_STATIC_LINKING "Enable static linking." ON)
+message(STATUS "[${PROJECT_NAME}] Enable static linking: ${RTCW_ENABLE_STATIC_LINKING}")
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-if (RTCW_ENABLE_STATIC_LINKING)
+if(RTCW_ENABLE_STATIC_LINKING)
 	set(RTCW_TMP_SDL2_COMPONENTS "static")
 	set(RTCW_TMP_SDL2_NET_COMPONENTS "static")
-else ()
+else()
 	set(RTCW_TMP_SDL2_COMPONENTS "")
 	set(RTCW_TMP_SDL2_NET_COMPONENTS "")
-endif ()
+endif()
 
 find_package(SDL2W 2.0.4 REQUIRED COMPONENTS ${RTCW_TMP_SDL2_COMPONENTS})
+find_package(SDL2W_net 2.0.1 REQUIRED COMPONENTS ${RTCW_TMP_SDL2_NET_COMPONENTS})
 
-find_package (SDL2W_net 2.0.1 REQUIRED COMPONENTS ${RTCW_TMP_SDL2_NET_COMPONENTS})
-
-if (RTCW_CURL)
-	find_package (CURL 7.0.0 REQUIRED)
-endif ()
+if(RTCW_CURL)
+	find_package(CURL 7.0.0 REQUIRED)
+endif()
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-function (rtcw_configure_target)
-	if (NOT (${ARGC} EQUAL 1))
-		message (FATAL_ERROR "Usage: rtcw_configure_target <target_name>")
-	endif ()
+function(rtcw_configure_target)
+	if(NOT (${ARGC} EQUAL 1))
+		message(FATAL_ERROR "Usage: rtcw_configure_target <target_name>")
+	endif()
 
-	set_target_properties (
-		${ARGV0}
-		PROPERTIES
-			CXX_STANDARD 98
-			CXX_STANDARD_REQUIRED ON
-			CXX_EXTENSIONS OFF
+	set_target_properties(${ARGV0} PROPERTIES
+		CXX_STANDARD 98
+		CXX_STANDARD_REQUIRED ON
+		CXX_EXTENSIONS OFF
 	)
 
-	if (NOT ((MSVC AND (MSVC_VERSION LESS 1600)) OR (CMAKE_CXX_COMPILER_ID STREQUAL "OpenWatcom")))
+	if(NOT ((MSVC AND (MSVC_VERSION LESS 1600)) OR (CMAKE_CXX_COMPILER_ID STREQUAL "OpenWatcom")))
 		set_target_properties(${ARGV0} PROPERTIES
 			COMPILE_FEATURES cxx_long_long_type
 		)
-	endif ()
+	endif()
 
-	get_target_property (RTCW_TMP_TARGET_TYPE ${ARGV0} TYPE)
+	get_target_property(RTCW_TMP_TARGET_TYPE ${ARGV0} TYPE)
 
 	unset(RTCW_TMP_IS_EXECUTABLE)
 
@@ -120,20 +113,18 @@ function (rtcw_configure_target)
 		set(RTCW_TMP_LINK_FLAGS "")
 	endif()
 
-	if (RTCW_TMP_TARGET_TYPE STREQUAL "EXECUTABLE")
+	if(RTCW_TMP_TARGET_TYPE STREQUAL "EXECUTABLE")
 		set(RTCW_TMP_IS_EXECUTABLE TRUE)
 
-		if (WIN32)
-			set_target_properties (
-				${ARGV0}
-				PROPERTIES
-					WIN32_EXECUTABLE TRUE
+		if(WIN32)
+			set_target_properties(${ARGV0} PROPERTIES
+				WIN32_EXECUTABLE TRUE
 			)
-		endif ()
+		endif()
 
 		############
 		# Stack size
-		if (WIN32)
+		if(WIN32)
 			if(MSVC)
 				set(RTCW_TMP_LINK_FLAGS "${RTCW_TMP_LINK_FLAGS} /STACK:0x800000")
 			elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
@@ -150,85 +141,73 @@ function (rtcw_configure_target)
 		if(MSVC AND (NOT (MSVC_VERSION LESS 1400)))
 			set(RTCW_TMP_LINK_FLAGS "${RTCW_TMP_LINK_FLAGS} /MANIFEST:NO")
 		endif()
-	endif ()
+	endif()
 
 	set_target_properties(${ARGV0} PROPERTIES LINK_FLAGS "${RTCW_TMP_LINK_FLAGS}")
 
-	target_compile_definitions (
-		${ARGV0}
+	target_compile_definitions(${ARGV0}
 		PRIVATE
 			__STDC_LIMIT_MACROS __STDC_CONSTANT_MACROS # stdint.h
 			__STDC_FORMAT_MACROS # inttypes.h
 			"RTCW_ARCH_STRING=\"${RTCW_ARCH_STRING}\""
 	)
 
-	if (RTCW_CURL AND RTCW_CURL_STATIC)
-		target_compile_definitions (
-			${ARGV0}
+	if(RTCW_CURL AND RTCW_CURL_STATIC)
+		target_compile_definitions(${ARGV0}
 			PRIVATE
 				CURL_STATICLIB
 		)
-	endif ()
+	endif()
 
-	if (RTCW_MULTI_PROCESS_COMPILATION)
-		if (MSVC)
-			target_compile_options (
-				${ARGV0}
+	if(RTCW_MULTI_PROCESS_COMPILATION)
+		if(MSVC)
+			target_compile_options(${ARGV0}
 				PRIVATE
 					-MP
 			)
-		endif ()
-	endif ()
+		endif()
+	endif()
 
-	if (RTCW_ENABLE_STATIC_LINKING)
-		if (MSVC)
-			target_compile_options(
-				${ARGV0}
+	if(RTCW_ENABLE_STATIC_LINKING)
+		if(MSVC)
+			target_compile_options(${ARGV0}
 				PRIVATE
 					$<$<CONFIG:DEBUG>:-MTd>
 					$<$<NOT:$<CONFIG:DEBUG>>:-MT>
 			)
-		endif ()
-	endif ()
+		endif()
+	endif()
 
-	if (RTCW_USE_PCH AND NOT (${CMAKE_VERSION} VERSION_LESS "3.16"))
-		target_precompile_headers(
-			${ARGV0}
-			PRIVATE
-				rtcw_pch.h
-		)
-	endif ()
+	if(RTCW_USE_PCH AND NOT (${CMAKE_VERSION} VERSION_LESS "3.16"))
+		target_precompile_headers(${ARGV0} PRIVATE rtcw_pch.h)
+	endif()
 
-	if (RTCW_CURL)
-		if (WIN32)
-			target_link_libraries (
-				${ARGV0}
+	if(RTCW_CURL)
+		if(WIN32)
+			target_link_libraries(${ARGV0}
 				PRIVATE
 					crypt32
 					wldap32
 			)
-		endif ()
+		endif()
 
-		target_link_libraries (
-			${ARGV0}
+		target_link_libraries(${ARGV0}
 			PRIVATE
 				CURL::libcurl
 		)
-	endif ()
+	endif()
 
-	if (RTCW_ENABLE_STATIC_LINKING)
-		if (WIN32)
-			target_link_libraries (
-				${ARGV0}
+	if(RTCW_ENABLE_STATIC_LINKING)
+		if(WIN32)
+			target_link_libraries(${ARGV0}
 				PRIVATE
 					ws2_32
 					iphlpapi
 			)
-		endif ()
-	endif ()
+		endif()
+	endif()
 
-	target_link_libraries (
-		${ARGV0}
+	target_link_libraries(${ARGV0}
 		PRIVATE
 			$<$<BOOL:${RTCW_TMP_IS_EXECUTABLE}>:SDL2W::SDL2Wmain>
 			SDL2W::SDL2W
@@ -236,46 +215,42 @@ function (rtcw_configure_target)
 			rtcw_lib_miniz
 			$<$<BOOL:${WIN32}>:winmm>
 	)
-endfunction (rtcw_configure_target)
+endfunction(rtcw_configure_target)
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-function (rtcw_configure_3rd_party_target)
-	if (NOT (${ARGC} EQUAL 1))
-		message (FATAL_ERROR "Usage: rtcw_configure_3rd_party_target <target_name>")
-	endif ()
+function(rtcw_configure_3rd_party_target)
+	if(NOT (${ARGC} EQUAL 1))
+		message(FATAL_ERROR "Usage: rtcw_configure_3rd_party_target <target_name>")
+	endif()
 
-	set_target_properties (
-		${ARGV0}
-		PROPERTIES
-			CXX_STANDARD 98
-			CXX_STANDARD_REQUIRED ON
-			CXX_EXTENSIONS OFF
+	set_target_properties(${ARGV0} PROPERTIES
+		CXX_STANDARD 98
+		CXX_STANDARD_REQUIRED ON
+		CXX_EXTENSIONS OFF
 	)
 
-	if (RTCW_MULTI_PROCESS_COMPILATION)
-		if (MSVC)
-			target_compile_options (
-				${ARGV0}
+	if(RTCW_MULTI_PROCESS_COMPILATION)
+		if(MSVC)
+			target_compile_options(${ARGV0}
 				PRIVATE
 					-MP
 			)
-		endif ()
-	endif ()
+		endif()
+	endif()
 
-	if (RTCW_ENABLE_STATIC_LINKING)
-		if (MSVC)
-			target_compile_options(
-				${ARGV0}
+	if(RTCW_ENABLE_STATIC_LINKING)
+		if(MSVC)
+			target_compile_options(${ARGV0}
 				PRIVATE
 					$<$<CONFIG:DEBUG>:-MTd>
 					$<$<NOT:$<CONFIG:DEBUG>>:-MT>
 			)
-		endif ()
-	endif ()
-endfunction (rtcw_configure_3rd_party_target)
+		endif()
+	endif()
+endfunction(rtcw_configure_3rd_party_target)
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
