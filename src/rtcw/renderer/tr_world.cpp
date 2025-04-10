ff0fed46b990b7da5bb2d0a10a780b6c035b0653
Boris I. Bendovsky <bibendovsky@hotmail.com>
2025-04-10 10:55:11 +0300

    Use SPDX identifier in license boilerplate

--- src/rtcw/renderer/tr_world.cpp
+++ src/rtcw/renderer/tr_world.cpp
@@ -1,29 +1,8 @@
 /*
-===========================================================================
-
-Return to Castle Wolfenstein single player GPL Source Code
-Copyright (C) 1999-2010 id Software LLC, a ZeniMax Media company. 
-
-This file is part of the Return to Castle Wolfenstein single player GPL Source Code (RTCW SP Source Code).  
-
-RTCW SP Source Code is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-RTCW SP Source Code is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with RTCW SP Source Code.  If not, see <http://www.gnu.org/licenses/>.
-
-In addition, the RTCW SP Source Code is also subject to certain additional terms. You should have received a copy of these additional terms immediately following the terms and conditions of the GNU General Public License which accompanied the RTCW SP Source Code.  If not, please request a copy in writing from id Software at the address below.
-
-If you have questions concerning this license or the applicable additional terms, you may contact in writing id Software LLC, c/o ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
-
-===========================================================================
+RTCW: Unofficial source port of Return to Castle Wolfenstein and Wolfenstein: Enemy Territory
+Copyright (C) 1999-2010 id Software LLC, a ZeniMax Media company.
+Copyright (c) 2012-2025 Boris I. Bendovsky bibendovsky@hotmail.com and Contributors
+SPDX-License-Identifier: GPL-3.0
 */
 
 #include "tr_local.h"
