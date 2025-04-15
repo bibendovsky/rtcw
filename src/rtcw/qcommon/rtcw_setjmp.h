fbc5fbeef3a771c91a3acef2c4cef07fa68fd235
Boris I. Bendovsky <bibendovsky@hotmail.com>
2025-04-15 21:22:25 +0300

    [MINGW] Fix setjmp for x86_64 Clang build

--- src/rtcw/qcommon/rtcw_setjmp.h
+++ src/rtcw/qcommon/rtcw_setjmp.h
@@ -7,18 +7,23 @@ SPDX-License-Identifier: GPL-3.0
 #ifndef RTCW_SETJMP_INCLUDED
 #define RTCW_SETJMP_INCLUDED
 
-#if defined(__MINGW32__) && defined(__GNUC__) && !defined(__llvm__) && (defined(__x86_64__) || defined(_M_X64))
-	#include <stdint.h>
+#if defined(__MINGW32__) && defined(__GNUC__) && (defined(__x86_64__) || defined(_M_X64))
+	#ifdef __llvm__
+		typedef void* RTCW_JMP_BUF[5];
+	#else // __llvm__
+		#include <stdint.h>
 
-	typedef intptr_t RTCW_JMP_BUF[5];
+		typedef intptr_t RTCW_JMP_BUF[5];
+	#endif // __llvm__
+
 	#define RTCW_SETJMP(env) __builtin_setjmp(env)
-	#define RTCW_LONGJMP(env,status) __builtin_longjmp(env,1)
+	#define RTCW_LONGJMP(env, status) __builtin_longjmp(env, 1)
 #else
 	#include <setjmp.h>
 
 	typedef jmp_buf RTCW_JMP_BUF;
 	#define RTCW_SETJMP(env) setjmp(env)
-	#define RTCW_LONGJMP(env,status) longjmp(env,status)
+	#define RTCW_LONGJMP(env, status) longjmp(env, status)
 #endif
 
 #endif // RTCW_SETJMP_INCLUDED
