e3652f12428e341f02da3eafa279938b46036c6c
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-02-25 21:58:29 +0200

    [no-exceptions] Revise rtcw_window_rounded_corner_mgr

--- src/rtcw/system/rtcw_window_rounded_corner_mgr.cpp
+++ src/rtcw/system/rtcw_window_rounded_corner_mgr.cpp
@@ -1,16 +1,17 @@
 /*
 RTCW: Unofficial source port of Return to Castle Wolfenstein and Wolfenstein: Enemy Territory
-Copyright (c) 2012-2025 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
+Copyright (c) 2025-2026 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors
 SPDX-License-Identifier: GPL-3.0
 */
 
 #include "rtcw_window_rounded_corner_mgr.h"
 #ifdef _WIN32
-#include <stddef.h>
-#include <string.h>
+#include "rtcw_memory.h"
+#include "rtcw_unique_ptr.h"
+#include <cstddef>
 #include <windows.h>
 #include "SDL_syswm.h"
-#include "rtcw_unique_ptr.h"
+
 #endif // _WIN32
 
 namespace rtcw {
@@ -29,8 +30,12 @@ private:
 	static const DWORD WIN32_DWMWCP_DONOTROUND = 1;
 	static const DWORD WIN32_DWMWA_WINDOW_CORNER_PREFERENCE = 33;
 
-private:
-	typedef UniquePtr<WindowRoundedCornerMgr::Impl, UniquePtrDefaultDeleter<WindowRoundedCornerMgr::Impl> > Instance;
+	struct Deleter
+	{
+		void operator()(Impl* impl) const;
+	};
+
+	typedef UniquePtr<WindowRoundedCornerMgr::Impl, Deleter> Instance;
 
 	typedef HRESULT (WINAPI * PFNDWMSETWINDOWATTRIBUTEPROC)(
 		HWND hwnd,
@@ -38,13 +43,11 @@ private:
 		LPCVOID pvAttribute,
 		DWORD cbAttribute);
 
-private:
 	static Instance instance_;
 
 	HMODULE dwmapi_module_;
 	PFNDWMSETWINDOWATTRIBUTEPROC dwm_set_window_attribute_;
 
-private:
 	Impl(const Impl&);
 	Impl& operator=(const Impl&);
 
@@ -53,11 +56,18 @@ private:
 	void disable(HWND win32_window) const;
 };
 
-// --------------------------------------------------------------------------
+// -------------------------------------
+
+void WindowRoundedCornerMgr::Impl::Deleter::operator()(Impl* impl) const
+{
+	mem::delete_object(impl);
+}
+
+// -------------------------------------
 
 WindowRoundedCornerMgr::Impl::Instance WindowRoundedCornerMgr::Impl::instance_;
 
-// --------------------------------------------------------------------------
+// -------------------------------------
 
 WindowRoundedCornerMgr::Impl::Impl()
 {
@@ -74,54 +84,45 @@ WindowRoundedCornerMgr::Impl::~Impl()
 
 void WindowRoundedCornerMgr::Impl::disable(SDL_Window* sdl_window)
 {
-	SDL_SysWMinfo sdl_sys_wm_info;
-	memset(&sdl_sys_wm_info, 0, sizeof(SDL_SysWMinfo));
+	SDL_SysWMinfo sdl_sys_wm_info = SDL_SysWMinfo();
 	SDL_VERSION(&sdl_sys_wm_info.version);
-
 	if (SDL_GetWindowWMInfo(sdl_window, &sdl_sys_wm_info) != SDL_TRUE ||
 		sdl_sys_wm_info.info.win.window == NULL)
 	{
 		return;
 	}
-
 	if (instance_.get() == NULL)
 	{
-		instance_.reset(new Impl());
+		instance_.reset(mem::new_object<Impl>());
 	}
-
 	instance_->disable(sdl_sys_wm_info.info.win.window);
 }
 
 bool WindowRoundedCornerMgr::Impl::initialize()
 {
-	dwmapi_module_ = LoadLibraryW(L"dwmapi.dll");
-
-	if (dwmapi_module_ == NULL)
-	{
-		return false;
-	}
-
 	union Cast
 	{
 		FARPROC from;
 		PFNDWMSETWINDOWATTRIBUTEPROC to;
 	};
 
+	dwmapi_module_ = LoadLibraryW(L"dwmapi.dll");
+	if (dwmapi_module_ == NULL)
+	{
+		return false;
+	}
 	const Cast cast = {GetProcAddress(dwmapi_module_, "DwmSetWindowAttribute")};
 	dwm_set_window_attribute_ = cast.to;
-
 	if (dwm_set_window_attribute_ == NULL)
 	{
 		return false;
 	}
-
 	return true;
 }
 
 void WindowRoundedCornerMgr::Impl::terminate()
 {
 	dwm_set_window_attribute_ = NULL;
-
 	if (dwmapi_module_ != NULL)
 	{
 		FreeLibrary(dwmapi_module_);
@@ -135,9 +136,7 @@ void WindowRoundedCornerMgr::Impl::disable(HWND win32_window) const
 	{
 		return;
 	}
-
 	const DWORD win32_rounded_corner_type = WIN32_DWMWCP_DONOTROUND;
-
 	dwm_set_window_attribute_(
 		win32_window,
 		WIN32_DWMWA_WINDOW_CORNER_PREFERENCE,
@@ -147,7 +146,7 @@ void WindowRoundedCornerMgr::Impl::disable(HWND win32_window) const
 
 #endif // _WIN32
 
-// ==========================================================================
+// =====================================
 
 #ifdef _WIN32
 
