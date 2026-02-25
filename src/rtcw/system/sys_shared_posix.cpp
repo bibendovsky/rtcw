b5111244aa8f63b8de33b468c3d59fc08919b48e
Boris I. Bendovsky <bibendovsky@hotmail.com>
2026-02-25 20:16:30 +0200

    [no-exceptions] Revise sys_shared

--- src/rtcw/system/sys_shared_posix.cpp
+++ src/rtcw/system/sys_shared_posix.cpp
@@ -1,36 +1,38 @@
 /*
 RTCW: Unofficial source port of Return to Castle Wolfenstein and Wolfenstein: Enemy Territory
 Copyright (C) 1999-2010 id Software LLC, a ZeniMax Media company.
-Copyright (c) 2012-2025 Boris I. Bendovsky bibendovsky@hotmail.com and Contributors
+Copyright (c) 2012-2026 Boris I. Bendovsky bibendovsky@hotmail.com and Contributors
 SPDX-License-Identifier: GPL-3.0
 */
 
 #ifndef _WIN32
 
 #include "sys_shared.h"
-#include <assert.h>
+#include "rtcw_memory.h"
+#include <cassert>
+#include <cstddef>
+#include <cstdlib>
+#include <cstring>
 #include <errno.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
-#include <new>
 #include <dirent.h>
 #include <time.h>
 #include <unistd.h>
 #include <sys/stat.h>
 
-// ==========================================================================
-
 namespace {
 
 template<typename T>
-void ignore_result(const T&) {}
+void maybe_unused(const T&)
+{}
 
-// ==========================================================================
+// =====================================
 
 class SysDirPosix
 {
 public:
+	SysDirPosix(const char* path);
+	~SysDirPosix();
+
 	static SysDirPosix* create(const char* path);
 	static void destroy(SysDirPosix* sys_dir);
 
@@ -39,63 +41,21 @@ public:
 private:
 	static const size_t max_path_size = 1024;
 
-private:
-	size_t name_index_;
+	std::size_t name_index_;
 	char path_[max_path_size];
 	DIR* posix_dir_;
 	struct dirent* posix_dirent_;
 	SysDirEntry entry_;
 
-private:
 	SysDirPosix(const SysDirPosix&);
 	SysDirPosix& operator=(const SysDirPosix&);
-
-private:
-	SysDirPosix(const char* path);
-	~SysDirPosix();
 
 	bool is_open() const;
 	void close();
 	bool update_entry();
 };
 
-// --------------------------------------------------------------------------
-
-SysDirPosix* SysDirPosix::create(const char* path)
-{
-	SysDirPosix* sys_dir = new SysDirPosix(path);
-
-	if (!sys_dir->is_open())
-	{
-		SysDirPosix::destroy(sys_dir);
-		return NULL;
-	}
-
-	return sys_dir;
-}
-
-void SysDirPosix::destroy(SysDirPosix* sys_dir)
-{
-	delete sys_dir;
-}
-
-const SysDirEntry* SysDirPosix::read()
-{
-	assert(is_open());
-	posix_dirent_ = ::readdir(posix_dir_);
-
-	if (posix_dirent_ == NULL)
-	{
-		return NULL;
-	}
-
-	if (!update_entry())
-	{
-		return NULL;
-	}
-
-	return &entry_;
-}
+// -------------------------------------
 
 SysDirPosix::SysDirPosix(const char* path)
 	:
@@ -110,19 +70,14 @@ SysDirPosix::SysDirPosix(const char* path)
 	else
 	{
 		// Copy whole path.
-		//
-		size_t path_size = strlen(path) + 1;
-
+		std::size_t path_size = std::strlen(path) + 1;
 		if (path_size >= max_path_size)
 		{
 			assert(false && "Path buffer too small.");
 			return;
 		}
-
-		memcpy(path_, path, path_size);
-
+		std::memcpy(path_, path, path_size);
 		// Append slash.
-		//
 		if (path_[path_size - 2] != '/')
 		{
 			if (path_size == max_path_size)
@@ -130,33 +85,58 @@ SysDirPosix::SysDirPosix(const char* path)
 				assert(false && "Path buffer too small.");
 				return;
 			}
-
 			path_[path_size - 1] = '/';
 			path_[path_size] = '\0';
 			++path_size;
 		}
-
 		// Append the dot.
-		//
 		if (path_size == max_path_size)
 		{
 			assert(false && "Path buffer too small.");
 			return;
 		}
-
 		path_[path_size - 1] = '.';
 		path_[path_size] = L'\0';
 		name_index_ = path_size - 1;
 	}
-
 	// Make API call.
-	//
-	posix_dir_ = ::opendir(path_);
+	posix_dir_ = opendir(path_);
 }
 
 SysDirPosix::~SysDirPosix()
 {
 	close();
+}
+
+SysDirPosix* SysDirPosix::create(const char* path)
+{
+	SysDirPosix* const sys_dir = rtcw::mem::new_object_1<SysDirPosix>(path);
+	if (sys_dir == NULL || !sys_dir->is_open())
+	{
+		SysDirPosix::destroy(sys_dir);
+		return NULL;
+	}
+	return sys_dir;
+}
+
+void SysDirPosix::destroy(SysDirPosix* sys_dir)
+{
+	rtcw::mem::delete_object(sys_dir);
+}
+
+const SysDirEntry* SysDirPosix::read()
+{
+	assert(is_open());
+	posix_dirent_ = readdir(posix_dir_);
+	if (posix_dirent_ == NULL)
+	{
+		return NULL;
+	}
+	if (!update_entry())
+	{
+		return NULL;
+	}
+	return &entry_;
 }
 
 bool SysDirPosix::is_open() const
@@ -171,47 +151,38 @@ void SysDirPosix::close()
 		return;
 	}
 
-	const int int_result = ::closedir(posix_dir_);
+	const int int_result = closedir(posix_dir_);
 	assert(int_result == 0);
-	ignore_result(int_result);
+	maybe_unused(int_result);
 	posix_dir_ = NULL;
 }
 
 bool SysDirPosix::update_entry()
 {
 	// Append the name to the path.
-	//
-	const size_t name_size = strlen(posix_dirent_->d_name) + 1;
-
+	const std::size_t name_size = std::strlen(posix_dirent_->d_name) + 1;
 	if (name_index_ + name_size > max_path_size)
 	{
 		assert(false && "Path buffer too small.");
 		return false;
 	}
-
-	memcpy(&path_[name_index_], posix_dirent_->d_name, name_size);
-
+	std::memcpy(path_ + name_index_, posix_dirent_->d_name, name_size);
 	// Get file info.
-	//
 	struct stat posix_stat;
-	const int int_result = ::stat(path_, &posix_stat);
-
+	const int int_result = stat(path_, &posix_stat);
 	if (int_result != 0)
 	{
 		return false;
 	}
-
-	
 	// Update the entry.
-	//
 	entry_.is_dir = S_ISDIR(posix_stat.st_mode) != 0;
-	entry_.name = &path_[name_index_];
+	entry_.name = path_ + name_index_;
 	return true;
 }
 
 } // namespace
 
-// ==========================================================================
+// =====================================
 
 SysDirHandle sys_open_dir(const char* path)
 {
@@ -228,63 +199,54 @@ void sys_close_dir(SysDirHandle& handle)
 	SysDirPosix::destroy(reinterpret_cast<SysDirPosix*>(handle));
 }
 
-// ==========================================================================
+// =====================================
 
 void Sys_Mkdir(const char* path)
 {
-	const int posix_result = ::mkdir(path, 0777);
-
+	const int posix_result = mkdir(path, 0777);
 #ifndef NDEBUG
 	if (posix_result != 0)
 	{
 		assert(errno == EEXIST);
 	}
 #endif
-
-	ignore_result(posix_result);
+	maybe_unused(posix_result);
 }
 
-// ==========================================================================
+// =====================================
 
 char* Sys_Cwd()
 {
 	const int max_path_size = 1024;
 	static char path[max_path_size];
-	char* posix_result = ::getcwd(path, max_path_size);
-
+	char* const posix_result = getcwd(path, max_path_size);
 	if (posix_result == NULL)
 	{
 		assert(false && "getcwd");
 		path[0] = '\0';
 	}
-
 	return path;
 }
 
-// ==========================================================================
+// =====================================
 
 int Sys_Milliseconds()
 {
 	static bool is_initialized = false;
 	static long long time_base_ms = 0;
-
 	struct timespec posix_timespec;
-	const int posix_result = ::clock_gettime(CLOCK_REALTIME, &posix_timespec);
-
+	const int posix_result = clock_gettime(CLOCK_REALTIME, &posix_timespec);
 	if (posix_result != 0)
 	{
 		return -1;
 	}
-
 	const long long current_time_ms = (posix_timespec.tv_sec * 1000000000LL + posix_timespec.tv_nsec) / 1000000LL;
-
 	if (!is_initialized)
 	{
 		is_initialized = true;
 		time_base_ms = current_time_ms;
 		return 0;
 	}
-
 	const long long diff_ms = current_time_ms - time_base_ms;
 	return static_cast<int>(diff_ms);
 }
